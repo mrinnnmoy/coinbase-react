fc18f51ce8943a36ec0cec641f9c41c88f84e412
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-11 19:23:27 +0530

    rebuild navbar-component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import "./Navbar.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
-import Button from "../Button/Button";
 
-export default function Navbar() {
+const Navbar = () => {
     const links = ["Home", "Dashboard", "Learn", "FAQ", "Contact", "About"];
     const [isNavOpen, setIsNavOpen] = useState(false);
     const html = document.querySelector("html");
     html.addEventListener("click", (e) => setIsNavOpen(false));
+
     return (
-        <Nav state={isNavOpen ? 1 : 0} className="flex j-between a-center">
+        <nav state={isNavOpen ? 1 : 0} className="flex j-between a-center">
             <div className="brand">
                 <h2>Coinbase</h2>
             </div>
@@ -18,12 +18,10 @@ export default function Navbar() {
                 {isNavOpen ? (
                     <MdClose onClick={() => setIsNavOpen(false)} />
                 ) : (
-                    <GiHamburgerMenu
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            setIsNavOpen(true);
-                        }}
-                    />
+                    <GiHamburgerMenu onClick={(e) => {
+                        e.stopPropagation();
+                        setIsNavOpen(true);
+                    }} />
                 )}
             </div>
             <div className={`links ${isNavOpen ? "show" : ""}`}>
@@ -38,92 +36,11 @@ export default function Navbar() {
                 </ul>
             </div>
             <div className="auth flex gap-1">
-                <Button text="Sign In" subduedButton />
-                <Button text="Sign Up" />
+                <button className="btn btn-1">Sign In</button>
+                <button className="btn">Sign Up</button>
             </div>
-        </Nav>
-    );
-}
+        </nav>
+    )
+};
 
-const Nav = styled.nav`
-  margin: 3rem 5rem;
-  .brand {
-    h2 {
-      font-size: 2.5rem;
-    }
-  }
-  .toggle {
-    display: none;
-  }
-  .links {
-    ul {
-      font-weight: bolder;
-      li {
-        cursor: pointer;
-        &:first-of-type {
-          a {
-            opacity: 1;
-          }
-        }
-        a {
-          opacity: 0.7;
-          transition: 0.5s ease-in-out;
-          &:hover {
-            opacity: initial;
-          }
-        }
-      }
-    }
-  }
-  @media screen and (min-width: 280px) and (max-width: 1080px) {
-    padding: 3rem 1rem;
-    margin: 0;
-    position: relative;
-    z-index: 5;
-    flex-direction: row;
-    .brand {
-      h2 {
-        font-size: 2rem;
-      }
-    }
-    .toggle {
-      padding-right: 1rem;
-      display: block;
-      z-index: 51;
-      svg {
-        color: #2d69fd;
-      }
-    }
-    .show {
-      opacity: 1 !important;
-      visibility: visible !important;
-    }
-    .links {
-      z-index: 10;
-      position: fixed;
-      overflow-x: hidden;
-      top: 0;
-      right: 0;
-      width: ${({ state }) => (state ? "60%" : "0%")};
-      height: 100vh;
-      background-color: white;
-      opacity: 0;
-      visibility: hidden;
-      transition: 0.7s ease-in-out;
-      ul {
-        flex-direction: column;
-        text-align: center;
-        height: 100%;
-        justify-content: center;
-        li {
-          a {
-            color: #2d69fd;
-          }
-        }
-      }
-    }
-    .auth {
-      display: none;
-    }
-  }
-`;
+export default Navbar;
