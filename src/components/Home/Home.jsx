6949a64d33927f4c6aa889e129fe7ae17f374e29
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-11 19:55:37 +0530

    build home-component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./Home.scss";
+import { FaChevronCircleRight } from "react-icons/fa";
+import home from "../../assets/home.png";
+import badgePercent from "../../assets/badge-percent.png";
+
+const Home = () => {
+    return (
+        <section className="flex j-center a-center gap">
+            <div className="content flex column gap-2">
+                <div className="subtitle">
+                    <h3 className="flex a-center gap-1 blue">
+                        <span>
+                            <img src={badgePercent} alt="badgepercent" />
+                        </span>{" "}
+                        Investment made easy
+                    </h3>
+                </div>
+                <div className="title">
+                    <h1>The Easiest Way to Track Multiple Currencies.</h1>
+                </div>
+                <div className="description">
+                    <p className="subdue">
+                        Market Watchman allow you tu monitor your balances, trade without,
+                        limits and earn rewards for specific coins.
+                    </p>
+                </div>
+                <div className="buttons flex gap-1">
+                    <button className="btn flex gap-1">
+                        Try Now
+                        <FaChevronCircleRight />
+                    </button>
+                    <button className="btn btn-1">How it works?</button>
+                </div>
+            </div>
+            <div className="image">
+                <img src={home} alt="home" className="half-width" />
+            </div>
+        </section>
+    )
+};
+
+export default Home;
